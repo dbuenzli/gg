b3a3154a5501d3540b8b04784ea7aa982a7d746f
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-02-14 23:24:16 +0100

    Install index.mld.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -8,5 +8,6 @@ let () =
   Ok [ Pkg.mllib "src/gg.mllib";
        Pkg.mllib ~api:[] "src/gg_top.mllib";
        Pkg.lib "src/gg_top_init.ml";
+       Pkg.doc "doc/index.mld" ~dst:"odoc-pages/index.mld";
 (*       Pkg.test "test/benchmark"; *)
        Pkg.test "test/test"; ]
